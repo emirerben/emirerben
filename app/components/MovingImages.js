b1e1e2631c388f7f6eb6e495861fc8d72a9a162b
erben <erben@umich.edu>
2024-01-14 11:49:12 +0300

    update

--- app/components/MovingImages.js
+++ app/components/MovingImages.js
@@ -1,47 +1,44 @@
-    // CSS in globals.css (or any other CSS file)
-  // MovingImages.js
-  'use client'
-  import { CldImage } from "next-cloudinary";
-  import { useEffect, useState } from "react";
+import Image from "next/image";
+import { CldImage } from "next-cloudinary";
+const cloudinary = require('cloudinary').v2
 
-  export default function MovingImages({ foldername, text }) {
-    const [images, setImages] = useState([]);
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.API_KEY_CLOUDINARY,
+  api_secret: process.env.API_SECRET_CLOUDINARY
+});
 
-    useEffect(() => {
-      const fetchImages = async () => {
-        try {
-          const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-          const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/resources/image/folder/${foldername}`);
-          const data = await response.json();
-          setImages(data.resources);
-        } catch (error) {
-          console.error('Error fetching images:', error);
-        }
-      };
+export default function MovingImages({ foldername, text }) {
+  // Assuming that your images follow a naming pattern like '1.jpg', '2.jpg', etc.
+  // Adjust the range according to the number of images in the folder
+  const numberOfImages = 12; // Number of images you have in the folder
+  const images = Array.from({ length: numberOfImages }, (_, index) => 
+    `https://res.cloudinary.com/dh4enklg7/image/upload/v1704691428/${foldername}/${index + 1}.jpg`
+  );
 
-      fetchImages();
-    }, [foldername]);
-
-    return (
-      <div className="w-full flex flex-col items-center justify-center">
-        <div className="grid grid-cols-3 sm:grid-cols-4 gap-2">
-          {images.map((image, index) => (
-            <CldImage
-              width="960"
-              key={index}
-              height="600"
-              src={image.secure_url}
-              sizes="100vw"
-              alt="Description of my image"
-              className="w-full h-auto"
-            />
-          ))} 
-        </div>
-        <div className="w-screen h-48 absolute overflow-clip overlow-hidden static">
-          <div className="overflow-hidden absolute text-white font-semibold text-6xl sm:text-[200px] animate-marquee whitespace-nowrap">
-            {text}
-          </div>
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-2">
+        {images.map((image, index) => (
+          <Image
+            width="960"
+            key={index}
+            height="600"
+            src={image}
+            sizes="100vw"
+            alt={`Image ${index}`}
+            className="w-full h-auto"
+          />
+        ))}
+      </div>
+      <div className="w-screen h-48 absolute overflow-clip overlow-hidden static">
+        <div className="overflow-hidden absolute text-white font-semibold text-9xl sm:text-[200px] animate-marquee whitespace-nowrap">
+          {text}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+
